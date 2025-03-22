68e4da19a71c3bc4e20952c40d14adbe64702606
SinghTanmay024 <sttanmay17@gmail.com>
2025-03-22 11:40:26 +0530

    User updated

--- src/main/java/com/winbid/backend/model/Bid.java
+++ src/main/java/com/winbid/backend/model/Bid.java
@@ -1,7 +1,6 @@
 package com.winbid.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
